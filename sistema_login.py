5cda03b4481927102eb9c79cb4c7ce48c7172a5a
Marcella Melo <marcelamelodejesus8@gmail.com>
2026-03-23 19:48:41 -0300

    Delete sistema_login.py

--- sistema_login.py
+++
@@ -1,8 +0,0 @@
-senha_mestra = "1234"
-while True:
-    senha = input("Digite a senha: ")
-    if senha == senha_mestra:
-        print("Acesso concedido!")
-        break
-    else:
-        print("Senha incorreta. Tente novamente.")
