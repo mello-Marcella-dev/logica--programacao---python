819535942dbb3898c95646488018b78562a88f06
Marcella Melo <marcelamelodejesus8@gmail.com>
2026-03-23 19:48:22 -0300

    Delete simulador_compras.py

--- simulador_compras.py
+++
@@ -1,8 +0,0 @@
-total = 0 
-while True:
-    valor = input("Qual o valor do produto? ou (Digite 'sair' para encerrar) ")
-    if valor.lower() == 'sair':
-        break
-    valor = float(valor)
-    total += valor
-print("O total da compra é: R$", total)
